046c95f49414834551aaa50acfdb3c3b953fd423
ak752 <ak752@DESKTOP-VOSGIAN>
2023-05-24 13:05:56 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿Random r = new();
+int[] mas = new int[14];
+for(int i =0; i < mas.Length; i++)
+    mas[i] = r.Next(-9, 10);
+for (int i = 0; i < mas.Length; i++)
+    Console.Write(mas[i] + " ");
+
+Console.WriteLine();
+
+int sum = 0, index = 0;
+while (mas[index] >= 0)
+{
+    sum += mas[index];
+    index++;
+};
+Console.WriteLine(sum);
